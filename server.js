f76c786df946612c5f3369b029ade6f57421c0c7
MicaSil <89428295+MicaSil@users.noreply.github.com>
2022-08-29 15:22:27 -0300

    Update server.js
    Re entrega

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res)=>{
-    const { min, nivel, max, titulo } = req.query;
-    res.render('vista', {min, nivel, max, titulo});
+app.get('/test', (req, res) =>{
+    const datos = {
+        min: 1,
+        max: 10,
+        value: 7,
+        titulo: 'Medidior de temperatura'
+    }
+    res.render('medidor', datos);
 });
 
-const PORT = 8080;
+app.get('/test', (req, res) =>{
+    const datos = {
+        min: req.query.min,
+        max: 10,
+        value: 7,
+        titulo: 'Medidor de temperatura'
+    }
+    res.render('medidor', datos);
+});
+
+const PORT = 5001;
 const server = app.listen(PORT, ()=>{
-    console.log(`Servidor esuchando en el puerto ${server.address().port}`)
-});
+    console.log(`Servidor escucando el puerto ${PORT}`);
+})
 
-server.on('error', err => console.log(`error en server ${err}`))
+server.on('error', error =>{
+    console.log(`Error en el servidor ${error}`)
+})
